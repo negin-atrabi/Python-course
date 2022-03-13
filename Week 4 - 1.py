e7f956a19cfd24f88a16f268ab642a887a2e15b2
negin-atrabi <100079017+negin-atrabi@users.noreply.github.com>
2022-03-14 01:14:37 +0330

    Add files via upload

---
+++ Week 4 - 1.py
@@ -0,0 +1,15 @@
+# the problem of the code was the gap line 10
+# true code :
+
+MAX = 20
+n = 1
+
+while n <= MAX:
+    factor = 1
+    print(end=str(n) + ': ')
+    while factor <= n:
+        if n % factor == 0:
+            print(factor, end=' ')
+        factor += 1
+    print()
+    n += 1
